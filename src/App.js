5a7c2c3025c21de169aa71ab175b1c4970f50e84
Abbie Weisenbloom <abbiew@froggie.com>
2018-10-12 22:26:19 -0700

    Passed heading parameters. Played with filters in App.js

--- src/App.js
+++ src/App.js
@@ -20,9 +20,19 @@ class BooksApp extends React.Component {
     BooksAPI.getAll()
     .then(response => {
     this.setState({books: response});
-    console.log(JSON.stringify(response))
-        console.log("hi" + JSON.stringify(response[0].title))
+   // console.log(JSON.stringify(response))
+    console.log("hi" + JSON.stringify(response[0].title));
+    console.log("hi" + JSON.stringify(response[0].shelf));
+    console.log(response.length)
    });
+  }
+
+  update = (book, shelf) => {    //update state.book attribute w/ correct shelf
+    BooksAPI.update(book, shelf)     // book {id}, shelf ["wantToRead", "currentlyReading", "read"]
+    .then(response => {
+    book.shelf = shelf; //set book shelf attribute to shelf
+    this.setState({books: response});
+    });
   }
 
   render() {
@@ -30,33 +40,38 @@ class BooksApp extends React.Component {
 
       <div className="app">
 
-          <div className="list-books">
+        <div className="list-books">
 
-            <div className="list-books-title">
+          <div className="list-books-title">
               <h1>MyReads</h1>
-            </div>
+          </div>
 
             <div className="list-books-content">
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div><Shelf/></div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div><Shelf/></div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div><Shelf/></div>
-                </div>
-            </div>
+              <div>
+                <Shelf
+                  update={this.update}
+                  name ="Currently Reading"
+                  books={this.state.books.filter(book => book.shelf === "currentlyReading")}
+                />
+                <Shelf
+                  update={this.update}
+                  name ="Want to Read"
+                  books={this.state.books.filter(book => book.shelf === "wantToRead")}
+                />
+                <Shelf
+                  update={this.update}
+                  name ="Read"
+                  books={this.state.books.filter(book => book.shelf === "read")}
+                />
+              </div>
+           </div>
 
-          </div>
+        </div>
 
           <div className="open-search">
             <Link to ="/search">Add a book</Link>
           </div>
-      </div>
+    </div>
     )
   }
 }
